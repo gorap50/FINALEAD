1c642580b420cb11cebdf2536fcae0e1424d7dd6
gorap50 <76959427+gorap50@users.noreply.github.com>
2023-06-24 16:50:31 +0500

    Recipi Details Code Added

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const recipiSchema = require("./models/recipi");
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/BookStore", {
+mongoose.connect("mongodb://127.0.0.1:27017/test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -19,11 +18,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.set("view engine", "ejs");
 
+app.post("/createrecipi", async (req, res) => {
+    try {
+      const { title, description, ingredients, instructions } = req.body;
+      const newRecipi = new recipiSchema({
+        title,
+        description,
+        ingredients,
+        instructions,
+      });
+      await newRecipi.save();
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
 app.get("/", async (req, res) => {
   try {
     const recipis = await recipiSchema.find();
-    console.log(recipis)
-    res.render("recipis", { recipis }); 
+    console.log(recipis);
+    res.render("recipis", { recipis });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/recipi/:id", async (req, res) => {
+  try {
+    const recipidtl = await recipiSchema.findById(req.params.id);
+    res.render("recipidetails.ejs", { recipi: recipidtl });
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
@@ -33,6 +59,8 @@ app.get("/", async (req, res) => {
 app.get("/createrecipi", (req, res) => {
   res.render("createrecipi");
 });
+
+
 
 app.listen(3005, () => {
   console.log("Server running on port 3005");
